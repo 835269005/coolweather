b81529f54b2291a44baef25be67a5758773d17ae
ylx <835269005@qq.com>
2019-05-20 14:50:49 +0800

    加入创建数据库和各项配配置.

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response)
+    {
+        if (!TextUtils.isEmpty(response))
+        {
+            try{
+                JSONArray allProvinces = new JSONArray(response;
+                
+
+            }
+
+        }
+    }
+}
