c30afd5a13e388c016ee6da8b163b667ab4529e2
ylx <835269005@qq.com>
2019-05-20 14:57:29 +0800

    加入创建数据库和表的各项配配置。

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -3,7 +3,10 @@ package com.coolweather.android.util;
 import android.text.TextUtils;
 import android.widget.TextView;
 
+import com.coolweather.android.db.Province;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Utility {
     public static boolean handleProvinceResponse(String response)
@@ -11,8 +14,13 @@ public class Utility {
         if (!TextUtils.isEmpty(response))
         {
             try{
-                JSONArray allProvinces = new JSONArray(response;
-                
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    Province.setProvinceName(provinceObject.getString("name"));
+                    Province
+                }
 
             }
 
